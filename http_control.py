44b97fa05b87a6b4e6c6296d81e0bdc90bf7ddfa
Matti Kariluoma <matti@kariluo.ma>
2014-08-16 16:06:43 -0700

    start with a use case, then we'll build from there

--- http_control.py
+++ http_control.py
@@ -13,7 +13,7 @@ After .start() is called, an http server is started in a thread. A
 web browser may then visit this server and provide new values for
 the application's registered state variables.
 
-It is the application's responsibility to use the .update() interface
+It is the application's responsibility to use the .get() interface
 to update its own internal state. This module will never attempt to
 write to any of the registered state variables.
 
@@ -22,7 +22,6 @@ write to any of the registered state variables.
 '''
 from __future__ import print_function
 import sys, datetime
-
 __version__ = '0.0'
 
 def debug(*objs):
@@ -30,11 +29,54 @@ def debug(*objs):
 	print('DEBUG: %s\n' % datetime.datetime.now(), *objs, file=sys.stderr)
 
 class Server():
+	supported_types = [int, float, str, list]
+	_type_not_implemented_msg = '''
+ {0} not supported.
+ You will need to manually convert it to and from one of: 
+ \t%s
+''' % '\n\t'.join([str(t) for t in supported_types])
+	
 	def __init__(self):
 		pass
+	def start(self):
+		pass
+	def stop(self):
+		pass
+	
+	def register(self, name, object_, type_=None):
+		'''
+		register a state variable with this server.
+		
+		name : the handle of the object, used for later .get() calls
+		object_ : the python object you want to remotely control
+		type_ : the type of object (only affects html generation)
+				e.g. bool, int, float, str, list, dict
+		'''
+		if type_ is None:
+			type_ = type(object_)
+		if type_ not in self.supported_types:
+			raise NotImplementedError(self._type_not_implemented_msg.format(type_))
+	
+	def unregister(self, name):
+		pass
+	def get(self, name):
+		pass
+	
 
 def demo():
-	debug('http_control %s' % __version__)
+	import time
+	debug('http_control version %s' % __version__)
+	running = True
+	msg = 'you can register before or after starting the server'
+	http_control_server = Server()
+	http_control_server.register('running', running, bool)
+	http_control_server.start()
+	http_control_server.register('msg', msg, str)
+	while running:
+		time.sleep(0.1)
+		msg = http_control_server.get('msg')
+		running = http_control_server.get('running')
+	http_control_server.stop()
 	
 if __name__ == '__main__':
 	demo()
